8b06936264388b66440c6c79d3d8d4fcb63ee863
cardo8819 <120418102+cardo8819@users.noreply.github.com>
2024-07-24 17:26:08 +0100

    Added the backend functionality and made the website responsive

--- script.js
+++ script.js
@@ -1,122 +1,167 @@
-const choose = ["Rock", "Paper", "Scissors"];
-const playerScoreDisplay = document.querySelector("#scores #player-score");
+//global elements
+const startButton = document.querySelector("#start-btn");
+const startContainer = document.querySelector("#start-container");
+const headerH1 = document.querySelector("header h1")
+const gameDisplay = document.querySelector("#game-display");
+const roundWinnerDisplay = document.querySelector("#game-display h2");
+const roundInfo = document.querySelector("#game-display h3");
+const playerScoreDisplay = document.querySelector("#player p");
+const computerScoreDisplay = document.querySelector("#computer p");
+const playerImage = document.querySelectorAll(".selection img");
+const displayModalBtn = document.createElement("button");
 
-const computerScoreDisplay = document.querySelector("#scores #computer-score");
-const choiceImg = document.querySelectorAll("#options img");
-const resultDiv = document.querySelector("#result");
-const resultP = document.createElement("p");
-resultDiv.appendChild(resultP);
+//audio
+const clickSound = document.querySelector("#start-click");
+const winAudio = document.querySelector("#win-audio");
+const loseAudio = document.querySelector("#lose-audio")
 
-let clickCount = 0;
-let gameRounds = 5;
-let humanScore = 0;
+//global variables
+const computerSelection = ["Rock", "Paper", "Scissors"];
+let roundWinner = "";
+let gameOver = false;
+let playerScore = 0;
 let computerScore = 0;
-playerScoreDisplay.textContent = `Player: ${humanScore}`;
-computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+let gameWinner = "";
 
 function getComputerChoice() {
-    return choose[Math.floor(Math.random() * choose.length)];
+    return computerSelection[Math.floor(Math.random() * computerSelection.length)];
 }
 
-
+startButton.addEventListener("click", () => {
+    const clickSound = document.querySelector("#start-click");
+    clickSound.play();
+    playGame();
+})
 
 function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    startContainer.style.display = "none";
+    gameDisplay.style.display = "flex";
+    headerH1.id = "animate"
 
-    function playRound(humanChoice, computerChoice) {
+    function playRound(playerSelection, computerSelection) {
 
-        if (humanChoice === computerChoice) {
-            resultP.textContent = `Its a Tie!! Your choice is ${humanChoice} the Computer's choice is ${computerChoice}`;
-        } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
-            humanScore++
-            resultP.textContent = `You Win!! ${humanChoice} beats ${computerChoice}`;
-            playerScoreDisplay.textContent = `Player: ${humanScore}`;
+        if (playerSelection === computerSelection) {
+            roundWinner = `Draw`;
         }
-        else if (humanChoice === "Paper" && computerChoice === "Rock") {
-            humanScore++
-            resultP.textContent = `You Win!! ${humanChoice} beats ${computerChoice}`;
-            playerScoreDisplay.textContent = `Player: ${humanScore}`;
+        else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            roundWinner = `Player`;
+            playerScore++;
         }
-        else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-            humanScore++
-            resultP.textContent = `You Win!! ${humanChoice} beats ${computerChoice}`;
-            playerScoreDisplay.textContent = `Player: ${humanScore}`;
+        else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            roundWinner = `Player`;
+            playerScore++;
+        }
+        else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            roundWinner = "Player";
+            playerScore++;
         }
         else {
-            computerScore++
-            resultP.textContent = `You Lose!! ${computerChoice} beats ${humanChoice}!`;
-            computerScoreDisplay.textContent = `Computer: ${computerScore}`
+            roundWinner = "Computer";
+            computerScore++;
+        }
+
+        updateScoreDisplay(roundWinner, playerSelection, computerSelection);
+        if (playerScore === 5 || computerScore === 5) {
+            gameOver = true;
+            toggleEventListeners()
+            displayResult()
         }
     }
 
-    choiceImg.forEach((img) => {
-        img.addEventListener("click", handleClick)
-    })
+    function updateScoreDisplay(winner, playerSelection, computerSelection) {
+        const playerImg = document.createElement("img");
+        const computerImg = document.createElement("img");
+        playerImg.src = `images/${(playerSelection.slice(0, 1).toLowerCase() + playerSelection.slice(1))}.png`;
+        computerImg.src = `images/${(computerSelection.slice(0, 1).toLowerCase() + computerSelection.slice(1))}.png`;
+        const playScoresCard = document.querySelector("#player .scores-card");
+        playScoresCard.innerHTML = `<img src=${playerImg.src}>`
+        const computerScoresCard = document.querySelector("#computer .scores-card");
+        computerScoresCard.innerHTML = `<img src=${computerImg.src}>`
+
+        playerScoreDisplay.textContent = `PLAYER: ${playerScore}`;
+        computerScoreDisplay.textContent = `COMPUTER: ${computerScore}`;
+
+        switch (winner) {
+            case "Player":
+                roundWinnerDisplay.textContent = `PLAYER Wins!`;
+                roundInfo.textContent = `${playerSelection} beats ${(computerSelection.slice(0, 1).toLowerCase() + computerSelection.slice(1))}!`
+                break;
+            case "Computer":
+                roundWinnerDisplay.textContent = `COMPUTER Wins!`;
+                roundInfo.textContent = `${computerSelection} beats ${(playerSelection.slice(0, 1).toLowerCase() + playerSelection.slice(1))}!`
+                break;
+            case "Draw":
+                roundWinnerDisplay.textContent = `Its a tie!`;
+                roundInfo.textContent = `${playerSelection} draws with ${(computerSelection.slice(0, 1).toLowerCase() + computerSelection.slice(1))}`
+        }
+    }
 
     function handleClick(e) {
-        
-        const getHumanChoice = e.target.id.slice(0,1).toUpperCase() + e.target.id.slice(1, e.target.length);
-        
-        
-        playRound(getHumanChoice, getComputerChoice());
-        clickCount++
-        if (clickCount === gameRounds) {
-            choiceImg.forEach((img) => {
-                img.removeEventListener("click", handleClick);
-            })
-            displayWinner();
-        }
+        const playerSelection = e.target.id;
+
+        const clickSound = document.querySelector("#start-click");
+        clickSound.play();
+        const getPlayerChoice = playerSelection;
+        playRound(getPlayerChoice, getComputerChoice())
+
     }
 
-
-    function displayWinner() {
-        if (humanScore > computerScore) {
-            resultP.textContent = `You Won! ${humanScore} : ${computerScore}`
-            
-        }
-        else if (humanScore === computerScore) {
-            resultP.textContent = `Its a Draw! ${humanScore} : ${computerScore}`
-            
-        }
-        else {
-            resultP.textContent = `You Lost! ${computerScore} : ${humanScore}`
-            
-        }
-        setTimeout(() => {
-            document.querySelector("#gameover").style.display = "flex";
-        }, 1000)
+    function toggleEventListeners() {
+        !gameOver ? playerImage.forEach((img) => img.addEventListener("click", handleClick)) : playerImage.forEach((img) => img.removeEventListener("click", handleClick))
     }
+    toggleEventListeners()
+
+
+    function displayResult() {
+        const container = document.querySelector("#container");
+        const displayModal = document.createElement("div");
+        displayModal.classList.add("modal");
+        const modalContent = document.createElement("div");
+        modalContent.classList.add("modal-content");
+        const displayModalH1 = document.createElement("h2");
+        const displayModalBtn = document.createElement("button");
+        displayModalBtn.id = "reset";
+        displayModalBtn.textContent = "Play Again";
+        if (computerScore > playerScore) {
+            displayModalH1.textContent = `You Lost! ${computerScore} : ${playerScore}`;
+            gameWinner = "Computer";
+        } else {
+            displayModalH1.textContent = `You Won! ${playerScore} : ${computerScore}`;
+            gameWinner = "Player"
+        }
+        modalContent.appendChild(displayModalH1);
+        modalContent.appendChild(displayModalBtn);
+        displayModal.appendChild(modalContent);
+        container.appendChild(displayModal);
+        const winAudio = document.querySelector("#win-audio");
+        const loseAudio = document.querySelector("#lose-audio")
+        gameWinner === "Player" ? winAudio.play() : loseAudio.play();
+        displayModalBtn.addEventListener("click", reset);
+    }
+
     function reset() {
-        clickCount = 0;
-        gameRounds = 5;
-        humanScore = 0;
+        document.querySelector(".modal").remove();
+        winAudio.currentTime = 0;
+        loseAudio.currentTime = 0;
+        winAudio.pause();
+        loseAudio.pause();
+        startContainer.style.display = "flex";
+        gameDisplay.style.display = "none";
+        roundWinner = "";
+        gameWinner = "";
+        gameOver = false;
+        playerScore = 0;
         computerScore = 0;
-        playerScoreDisplay.textContent = `Player: ${humanScore}`;
-        computerScoreDisplay.textContent = `Computer: ${computerScore}`;
-        
-
-        resultP.textContent = "";
-        document.querySelector("#gameover").style.display = "none";
-        choiceImg.forEach((img) => {
-            img.addEventListener("click", handleClick);
-
-        })
-
+        roundWinnerDisplay.textContent = `Make Your choice`
+        roundInfo.textContent = "First to score 5 Wins!";
+        playerScoreDisplay.textContent = `PLAYER: 0`;
+        computerScoreDisplay.textContent = `COMPUTER: 0`;
+        const resetPlayerScoreCardImg = document.querySelector("#player .scores-card img");
+        resetPlayerScoreCardImg.remove()
+        const resetComputerScoreCardImg = document.querySelector("#computer .scores-card img");
+        resetComputerScoreCardImg.remove();
+        headerH1.removeAttribute("id");
     }
-
-    document.querySelector("#gameover button").addEventListener("click", reset)
 }
-
-
-
-
-
-
-
-
-playGame();
-
-
-
-
-
-
